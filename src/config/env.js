1a4a2f4c2901f82fcf902cb6d61e60cfaf2aeb75
yu.wang <yu.wang@viteusa.com>
2021-08-17 20:54:41 -0400

    cert adjust

--- src/config/env.js
+++ src/config/env.js
@@ -3,22 +3,6 @@
 let firebaseConfig = JSON.parse(process.env.FIREBASE_CONFIG)
 let envType, url, eeveeApi, transactionFeeRate = 0.03
 switch(firebaseConfig.projectId) {
-  case 'easygo-core':
-    envType = 'core'
-    url = 'https://easygo-core.firebaseapp.com'
-    eeveeApi= {
-      apiKey: 've00XEHIRh7ztSrNE3ulQ1FE7juXBmWY8BHd3pbi',
-      url: 'https://test-api.vitedirect.com/'
-    }
-    break
-  case 'viteusa-prod':
-    envType = 'production'
-    url = 'https://app.viteusa.com'
-    eeveeApi= {
-      apiKey: '73rOM9fzWI7KGk9NRvBU54KNoHs2apfd9TVlN6q4',
-      url: 'https://api.vitedirect.com/'
-    }
-    break
   default:
     envType = 'development'
     url = 'http://localhost:8080'
--- src/config/stripe_auth.js
+++ src/config/stripe_auth.js
@@ -1,6 +1,5 @@
 'use strict'
 
 module.exports = {
-  testKey: 'sk_test_Jkd2M5opJ0nakhUgyxFy3YQ7001mQGFE2H',
-  liveKey: 'sk_live_XAQ8qds3te82Wp04YlodWWLO00GW3c3PLP'
+
 }
